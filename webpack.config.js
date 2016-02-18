9af7231b0c44443b329d20d55181e72e3ab17d19
Lego Chiang <lego@itnchu>
2016-02-18 22:08:57 +0800

    Doing

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require("webpack");
+module.exports = {
+  entry: {
+    vendor: ['react', 'react-dom', 'whatwg-fetch'],
+    app: __dirname + '/js/src/app.js',
+  },
+  output: {
+    path: __dirname + '/js/dist/',
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015','react', 'stage-0']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+  ]
+};
